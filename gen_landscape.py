60f73bcb8775585a3bc4a65fff4abde4e279005b
s.saroya@lse.ac.uk <lmp03300730>
2019-11-15 22:51:45 +0000

    Okay, worked on own perlin noise implementation. Took ages to make work. I think the core of it is now done. The next stage is to experiment with different values, ensure it works for higher dimensions, and then package the whole thing so it fits nicely into the rest of the program.

--- gen_landscape.py
+++ gen_landscape.py
@@ -1,4 +1,36 @@
+import noise
 import numpy as np
+from PIL import Image
+
+
+shape = (500, 500)
+scale = 100.0
+octaves = 1
+persistence = 0.5
+lacunarity = 2.0
+
+world = np.zeros(shape)
+for i in range(shape[0]):
+    for j in range(shape[1]):
+        world[i][j] = noise.pnoise2(i / scale,
+                                    j / scale,
+                                    octaves=octaves,
+                                    persistence=persistence,
+                                    lacunarity=lacunarity,
+                                    repeatx=1024,
+                                    repeaty=1024,
+                                    base=0)
+
+from matplotlib import pyplot as plt
+plt.imshow(world, interpolation='nearest')
+plt.show()
+world = np.asarray(world)
+world = (world * 100) + 100
+print(np.min(world))
+print(np.max(world))
+landscape = world
+
+
 def generate_landscape(size = 600):
     #Right now we also assume that pop fitness never goes beyond 600 - this might eb a bad assumption
     #Right now this has to be disgned for each loci number, currently for two
@@ -9,6 +41,5 @@ def generate_landscape(size = 600):
     landscape_ground = tuple([size]*loci)
     #we add the one to avoid 0 fitness
     landscape = np.fromfunction(lambda a, b: (a+b)**4, landscape_ground)
-    np.save('landscape.npy',landscape)
 
-generate_landscape()
+np.save('landscape.npy',landscape)
---
+++ perlin_noise.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+#we start by doing the 1d case:
+landscape_ground = np.zeros(100)
+print(landscape_ground)
