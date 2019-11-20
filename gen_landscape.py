8226cdf6a785ccf295ea7cda3e1d2e66dcebbfe3
s.saroya@lse.ac.uk <lmp03300730>
2019-11-20 17:42:24 +0000

    Okay. Ditched the perlin noise approach for simplex interpolation. This is simpler and easier to justify. Wrote up gen_landscape script. Generalised for n dimensions. We are looking set to go.

--- gen_landscape.py
+++ gen_landscape.py
@@ -1,45 +1,60 @@
-import noise
+import matplotlib.pyplot as plt
+import matplotlib.animation
+import seaborn as sns
 import numpy as np
-from PIL import Image
+import json
+import itertools
+from scipy.interpolate import griddata
+import pylab as p
+import mpl_toolkits.mplot3d.axes3d as p3
 
 
-shape = (500, 500)
-scale = 100.0
-octaves = 1
-persistence = 0.5
-lacunarity = 2.0
+# %%
+class Landscape():
+    def __init__(self, num_dimensions, dimension_size, num_grid_res=7):
+        self.num_dims = num_dimensions
+        self.dim_size = dimension_size
+        self.num_grid_res = num_grid_res
+        self.chosen_points = self.gen_chosen_points()
+        self.fitness_grid = self.gen_landscape()
 
-world = np.zeros(shape)
-for i in range(shape[0]):
-    for j in range(shape[1]):
-        world[i][j] = noise.pnoise2(i / scale,
-                                    j / scale,
-                                    octaves=octaves,
-                                    persistence=persistence,
-                                    lacunarity=lacunarity,
-                                    repeatx=1024,
-                                    repeaty=1024,
-                                    base=0)
+    def gen_chosen_points(self):
+        k = complex(0, self.num_grid_res)
+        grid_x, grid_y = np.mgrid[0:self.dim_size:k, 0:self.dim_size:k]
+        return np.vstack((grid_x.ravel(), grid_y.ravel())).T
 
-from matplotlib import pyplot as plt
-plt.imshow(world, interpolation='nearest')
-plt.show()
-world = np.asarray(world)
-world = (world * 100) + 100
-print(np.min(world))
-print(np.max(world))
-landscape = world
+    def uniform_rand_fit_assign(self, min=1, max=100):
+        num_rand_points = self.chosen_points.shape[0]
+        rand_fitness_array = np.random.random_integers(min, max, num_rand_points)
+        return rand_fitness_array
 
+    def gen_landscape(self):
+        rand_fitness_array = self.uniform_rand_fit_assign()
+        grid_x, grid_y = np.mgrid[0:self.dim_size, 0:self.dim_size]
+        return griddata(self.chosen_points, rand_fitness_array, (grid_x, grid_y), method='cubic')
 
-def generate_landscape(size = 600):
-    #Right now we also assume that pop fitness never goes beyond 600 - this might eb a bad assumption
-    #Right now this has to be disgned for each loci number, currently for two
-    #this is because of the lambda function
-    loci = 2
-    if size ==0:
-        size = int(input('What is the size of landscape'))
-    landscape_ground = tuple([size]*loci)
-    #we add the one to avoid 0 fitness
-    landscape = np.fromfunction(lambda a, b: (a+b)**4, landscape_ground)
+    def draw_2d_in_3d(self, elevation_deg=80, rotation_deg=0):
+        grid_x, grid_y = np.mgrid[0:self.dim_size, 0:self.dim_size]
+        fig = p.figure()
+        ax = p3.Axes3D(fig)
+        # ax.plot_wireframe(grid_x,grid_y,self.fitness_grid, color = 'black')
+        ax.contour3D(grid_x, grid_y, self.fitness_grid, 50, cmap=plt.cm.viridis)
+        ax.set_xlabel('loci_0')
+        ax.set_ylabel('loci_1')
+        ax.set_zlabel('Fitness')
+        ax.view_init(elevation_deg, rotation_deg)
+        p.show()
 
-np.save('landscape.npy',landscape)
+    def draw_2d(self):
+        plt.plot(self.chosen_points[:, 0], self.chosen_points[:, 1], 'k.', ms=10)
+        plt.imshow(self.fitness_grid.T, extent=(0, self.dim_size, 0, self.dim_size), origin='lower')
+        plt.title('Cubic')
+        plt.gcf().set_size_inches(10, 10)
+        plt.show()
+
+    def draw_2d_sea(self):
+        grid_x, grid_y = range(self.dim_size), range(self.dim_size)
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        ax.plot_trisurf(grid_x, grid_y, self.fitness_grid, cmap=plt.cm.viridis, linewidth=0.2)
+        plt.show()
